5c80e1f20248bb927247a483977237b3d26f10ef
Andy von Dohren <avondohren@gmail.com>
2014-12-18 20:47:18 -0600

    Update expire-sticky-posts.php

--- expire-sticky-posts.php
+++ expire-sticky-posts.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Expire Sticky Posts
  * Plugin URL: http://andyv.me
- * Description: A simple plugin that allows you to set an expiration date on posts. Once a post is expired, "Expired" it will no longer be sticky.
+ * Description: A simple plugin that allows you to set an expiration date on posts. Once a post is expired, it will no longer be sticky.
  * Version: 1.0
  * Author: Andy von Dohren
  * Author URI: http://andyv.me
